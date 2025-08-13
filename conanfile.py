769d25fbfca27cabfefba3818f6df1bfdd5f6834
Francisco Puerta <fpuerta@werfen.com>
2025-08-13 15:14:39 +0200

    Add Santuario (xsec) library

---
+++ conanfile.py
@@ -0,0 +1,31 @@
+from conans import ConanFile, tools
+
+
+class XsecConan(ConanFile):
+	name = "xsec"
+	description = "C++ cryptography library"
+	author = "CSW <csw@werfen.com>"
+	topics = ("conan", "crypto", "key")
+	generators = "visual_studio"
+	settings = "os", "compiler", "build_type", "arch"
+	options = {"xerces_char_type" : ['uint16_t', 'wchar_t']}
+	default_options = "openssl:shared=True", "xerces-c:shared=True", "xerces_char_type=uint16_t"
+
+	def requirements(self):
+		self.requires("zlib/1.3.1@#f52e03ae3d251dec704634230cd806a2", override = True)
+		self.requires("openssl/3.0.15@#05e3fb00d6d340c1c241a7347f0a9ec9")
+		self.requires("xerces-c/3.2.5#eaa4f49d6f4fa9351c8b706957ef0a99")
+
+	def config_options(self):
+		self.options["xerces-c"].char_type = self.options.xerces_char_type
+
+	def package(self):
+		self.copy("*.hpp", dst="include/xsec", src="xsec")
+		self.copy("*.h", dst="include/xsec", src="xsec")
+		self.copy("xsec_lib.lib", dst="lib", src=("Build/Win32/VC%s/%s Minimal" % (self.settings.compiler.version, self.settings.build_type)))
+		self.copy("xsec_lib.dll", dst="bin", src=("Build/Win32/VC%s/%s Minimal" % (self.settings.compiler.version, self.settings.build_type)))
+		self.copy("xsec_lib.pdb", dst="bin", src=("Build/Win32/VC%s/%s Minimal" % (self.settings.compiler.version, self.settings.build_type)))
+
+	def package_info(self):
+		self.cpp_info.libs = tools.collect_libs(self)
+		self.cpp_info.bindirs = ['bin']
---
+++ xsec/transformers/TXFMXSL.hpp
@@ -0,0 +1,121 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+/*
+ * XSEC
+ *
+ * TXFMXSL := Class that performs XML Stylesheet Language transforms
+ *
+ * $Id: TXFMXSL.hpp 1894293 2021-10-15 14:14:50Z scantor $
+ *
+ */
+
+#include <xsec/transformers/TXFMBase.hpp>
+
+// Xerces
+
+#include <xercesc/dom/DOM.hpp>
+
+// Xalan
+
+#ifdef XSEC_HAVE_XPATH
+
+#include <xalanc/XalanDOM/XalanDocument.hpp>
+#include <xalanc/XercesParserLiaison/XercesDOMSupport.hpp>
+#include <xalanc/XercesParserLiaison/XercesParserLiaison.hpp>
+#include <xalanc/XPath/NodeRefList.hpp>
+#include <xalanc/XPath/ElementPrefixResolverProxy.hpp>
+#include <xalanc/XalanTransformer/XalanTransformer.hpp>
+
+// If this isn't defined, we're on Xalan 1.12+ and require modern C++
+#ifndef XALAN_USING_XALAN
+# define XALAN_USING_XALAN(NAME) using xalanc :: NAME;
+#endif
+
+// Xalan Namespace usage
+XALAN_USING_XALAN(XercesDOMSupport)
+XALAN_USING_XALAN(XercesParserLiaison)
+XALAN_USING_XALAN(XalanDocument)
+XALAN_USING_XALAN(XalanTransformer)
+
+#endif
+
+#ifdef XSEC_HAVE_XSLT
+
+/**
+ * \brief Transformer to handle XSLT transforms
+ * @ingroup internal
+ */
+
+class XSEC_EXPORT TXFMXSL : public TXFMBase {
+
+private:
+
+	safeBuffer expr;							// The expression being worked with
+
+	XercesDOMSupport	xds;
+	XercesParserLiaison xpl;
+
+	XalanDocument		* xd;
+
+	safeBuffer			sbInDoc;
+
+	XERCES_CPP_NAMESPACE_QUALIFIER DOMDocument		
+						* document;
+	
+	XERCES_CPP_NAMESPACE_QUALIFIER DOMDocument		
+						* docOut;			// The output from the transformation
+
+public:
+
+	TXFMXSL(XERCES_CPP_NAMESPACE_QUALIFIER DOMDocument *doc);
+	virtual ~TXFMXSL();
+
+	// Methods to set the inputs
+
+	void setInput(TXFMBase *newInput);
+	
+	// Methods to get tranform output type and input requirement
+
+	virtual TXFMBase::ioType getInputType(void) const;
+	virtual TXFMBase::ioType getOutputType(void) const;
+	virtual nodeType getNodeType(void) const;
+
+	// We do our own name spaces - we have a new document!
+	
+	virtual bool nameSpacesExpanded(void) const;
+	virtual void expandNameSpaces(void);
+
+
+	// XSL Unique
+
+	void evaluateStyleSheet(const safeBuffer &sbStyleSheet);
+
+	// Methods to get output data
+
+	virtual unsigned int readBytes(XMLByte * const toFill, const unsigned int maxToFill);
+	virtual XERCES_CPP_NAMESPACE_QUALIFIER DOMDocument *getDocument() const;
+	
+private:
+	TXFMXSL();
+
+};
+
+
+#endif /* No XSLT */
